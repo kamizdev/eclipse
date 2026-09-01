aa22699ab0bc6cb93ff3839d212fc364c115afba
kamizdev <fede.camisa07@gmail.com>
2026-09-01 09:42:37 +0200

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,84 @@
+{
+  "songs": [
+
+    {
+      "id": "canzone-1",
+
+      "title": "Canzone 1",
+
+      "description": "6 stem",
+
+      "folder": "songs/canzone-1",
+
+      "stems": [
+
+        {
+          "name": "Vocals",
+          "file": "vocals.wav"
+        },
+
+        {
+          "name": "Backing Vocals",
+          "file": "backing-vocals.wav"
+        },
+
+        {
+          "name": "Guitar",
+          "file": "guitar.wav"
+        },
+
+        {
+          "name": "Bass",
+          "file": "bass.wav"
+        },
+
+        {
+          "name": "Drums",
+          "file": "drums.wav"
+        },
+
+        {
+          "name": "Keys",
+          "file": "keys.wav"
+        }
+
+      ]
+    },
+
+
+    {
+      "id": "canzone-2",
+
+      "title": "Canzone 2",
+
+      "description": "4 stem",
+
+      "folder": "songs/canzone-2",
+
+      "stems": [
+
+        {
+          "name": "Vocals",
+          "file": "vocals.mp3"
+        },
+
+        {
+          "name": "Guitar",
+          "file": "guitar.mp3"
+        },
+
+        {
+          "name": "Bass",
+          "file": "bass.mp3"
+        },
+
+        {
+          "name": "Drums",
+          "file": "drums.mp3"
+        }
+
+      ]
+    }
+
+  ]
+}
